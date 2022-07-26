23798909df4cd1a32855ab3d115aa598522a24e5
Meghna Patel <meghna10jan21@yahoo.com>
2022-07-25 21:33:20 -0500

    Adding 404 page view

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.engine('jsx', require ('express-react-views').createEngine())
 app.use('/places', require('./controllers/places'))
 
 app.get('/', (req, res) => {
-    res.render('home')
+    res.render('error404')
 })
 
 app.get('*', (req, res) => {
