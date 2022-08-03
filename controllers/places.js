71ec088fb66b4f9d07c63a1ecce59a277a289713
Meghna Patel <meghna10jan21@yahoo.com>
2022-08-03 14:48:17 -0500

    show route connected

--- controllers/places.js
+++ controllers/places.js
@@ -1,22 +1,17 @@
 const router = require ('express').Router()
 const router = require ('../Models/places.js')
 
-router.get ('/', (req, res) => {
-    let places = [{
-        name: 'H-Thai-ML',
-        city: 'Seattle',
-        state: 'WA',
-        cuisines: 'Thai, Pan-Asian',
-        pic: '/images/h-thai-ml-tables.png'
-      }, {
-        name: 'Coding Cat Cafe',
-        city: 'Phoenix',
-        state: 'AZ',
-        cuisines: 'Coffee, Bakery',
-        pic: '/images/coffee-cat.jpg'
-      }]
-      
-    res.render ('places/index', {places})
+router.get ('/:id', (req, res) => {
+    let id = Number (req.params.id)
+    if (isNaN(id)){
+    res.render('error404')
+}
+else if(!places[id]){
+  res.render('error404')
+}
+else {
+  res.render('places/show', {place: places[id]})
+}  
 })
 
   router.post('/', (req, res) => {
