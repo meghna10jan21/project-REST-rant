419b5ed711fb59f14541e10e3843c461bfb94c58
Meghna Patel <meghna10jan21@yahoo.com>
2022-08-12 11:03:05 -0500

    part-9

--- controllers/places.js
+++ controllers/places.js
@@ -1,57 +1,105 @@
-const router = require('express').Router()
-const db = require('../models')
+const router = require("express").Router();
+const db = require("../models");
 
-router.get('/:id', (req, res) => {
-  db.Place.findById(req.params.id)
-  .then(place => {
-      res.render('places/show', { place })
-  })
-  .catch(err => {
-      console.log('err', err)
-      res.render('error404')
-  })
-})
+//INDEX
+router.get("/", (req, res) => {
+  db.Place.find()
+    .then((places) => {
+      res.render("places/index", { places });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.render("error404");
+    });
+});
 
+//CREATE
+router.post("/", (req, res) => {
+  
 
-
-router.post('/', (req, res) => {
   db.Place.create(req.body)
-  .then(() => {
-      res.redirect('/places')
-  })
-  .catch(err => {
-      console.log('err', err)
-      res.render('error404')
-  })
-})
+    .then(() => {
+      res.redirect("/places");
+    })
+    .catch((err) => {
+      if (err && err.name == "ValidationError") {
+        let message = "Validation Error: ";
+        for (var field in err.errors) {
+          message += `${field} was ${err.errors[field].value}. `;
+          message += `${err.errors[field].message}`;
+        }
+        console.log("Validation error message", message);
+        res.render("places/new", { message });
+      } else {
+        res.render("error404");
+      }
+    });
+});
 
+//NEW
+router.get("/new", (req, res) => {
+  res.render("places/new");
+});
 
-router.get('/new', (req, res) => {
-  res.render('places/new')
-})
+//GET BY ID
+router.get("/:id", (req, res) => {
+  db.Place.findById(req.params.id)
+    .then((place) => {
+      res.render("places/show", { place });
+    })
+    .catch((err) => {
+      console.log("err", err);
+      res.render("error404");
+    });
+});
 
-router.get('/:id', (req, res) => {
-  res.send('GET /places/:id stub')
-})
-
+//UPDATE
 router.put('/:id', (req, res) => {
-  res.send('PUT /places/:id stub')
+  db.Place.findByIdAndUpdate(req.params.id, req.body)
+      .then(() => {
+          res.redirect(`/places/${req.params.id}`)
+      })
+      .catch(err => {
+          console.log('err', err)
+          res.render('error404')
+      })
 })
 
 router.delete('/:id', (req, res) => {
-  res.send('DELETE /places/:id stub')
+  db.Place.findByIdAndDelete(req.params.id)
+      .then(() => {
+          res.redirect('/places')
+      })
+      .catch(err => {
+          console.log('err', err)
+          res.render('error404')
+      })
 })
 
 router.get('/:id/edit', (req, res) => {
-  res.send('GET edit form stub')
+  db.Place.findById(req.params.id)
+      .then(place => {
+          res.render('places/edit', { place })
+      })
+      .catch(err => {
+          res.render('error404')
+      })
 })
 
-router.post('/:id/rant', (req, res) => {
-  res.send('GET /places/:id/rant stub')
-})
+router.delete("/:id", (req, res) => {
+  res.send("DELETE /places/:id stub");
+});
 
-router.delete('/:id/rant/:rantId', (req, res) => {
-    res.send('GET /places/:id/rant/:rantId stub')
-})
+router.get("/:id/edit", (req, res) => {
+  res.send("GET edit form stub");
+});
 
-module.exports = router
+router.post("/:id/rant", (req, res) => {
+  res.send("GET /places/:id/rant stub");
+});
+
+router.delete("/:id/rant/:rantId", (req, res) => {
+  res.send("GET /places/:id/rant/:rantId stub");
+});
+
+module.exports = router;
--- views/places/show.jsx
+++ views/places/show.jsx
@@ -1,25 +1,54 @@
-const React = require('react')
-const Def = require('../default')
+const React = require("react");
+const Def = require("../default");
 
-function show (data) {
-    return (
-        <Def>
-          <main>
+function show(data) {
+  return (
+    <Def>
+      <main>
+        <div className="row">
+          <div className="col-sm-6">
+            <img
+              className="col-sm-6"
+              src={data.place.pic}
+              alt={data.place.name}
+            />
+            <h3>
+              Located in {data.place.city}, {data.place.state}
+            </h3>
+          </div>
+          <div className="col-sm-6">
             <h1>{data.place.name}</h1>
             <h2>Rating</h2>
-            <h3>Comments</h3>
-            <a href="" className="btn btn-warning"> 
-            Edit
-            </a>  
-          <form method="POST" action={`/places/${data.id}?_method=DELETE`}> 
-          <button type="submit" className="btn btn-danger">
-          Delete
-          </button>
-        </form>     
-
-          </main>
-        </Def>
-    )
+            <br />
+            <h2>Description</h2>
+            <h3>{data.place.showEstablished()}</h3>
+            <h4>Serving {data.place.cuisines}</h4>
+            <br />
+            <a
+              href={`/places/${data.place.id}/edit`}
+              className="btn btn-warning"
+            >
+              Edit
+            </a>
+            {` `}
+            <form
+              method="POST"
+              action={`/places/${data.place.id}?_method=DELETE`}
+            >
+              <button type="submit" className="btn btn-danger">
+                Delete
+              </button>
+            </form>
+          </div>
+        </div>
+        <hr />
+            <h2>Comments</h2>
+            <div className="row">
+              
+            </div>
+      </main>
+    </Def>
+  );
 }
 
-module.exports = show
+module.exports = show;
