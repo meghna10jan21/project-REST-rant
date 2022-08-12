6f5afd7ce0a6599fc508bdffa0c23ee3a067b41f
Meghna Patel <meghna10jan21@yahoo.com>
2022-08-12 15:20:00 -0500

    part-10

--- controllers/places.js
+++ controllers/places.js
@@ -2,42 +2,39 @@ const router = require("express").Router();
 const db = require("../models");
 
 //INDEX
-router.get('/:id', (req, res) => {
-  db.Place.findById(req.params.id)
-  .populate('comments')
-  .then(place => {
-    console.log(place.comments)
-      res.render('places/show', { place })
-  })
-  .catch(err => {
-      console.log('err', err)
-      res.render('error404')
-  })
+router.get('/', (req, res) => {
+  db.Place.find()
+      .then((places) => {
+          res.render('places/index', { places })
+      })
+      .catch(err => {
+          console.log('err', err)
+          res.render('error404')
+      })
 })
 
 
 //CREATE
-router.post("/", (req, res) => {
-  
-
-  db.Place.create(req.body)
-    .then(() => {
-      res.redirect("/places");
-    })
-    .catch((err) => {
-      if (err && err.name == "ValidationError") {
-        let message = "Validation Error: ";
-        for (var field in err.errors) {
-          message += `${field} was ${err.errors[field].value}. `;
-          message += `${err.errors[field].message}`;
-        }
-        console.log("Validation error message", message);
-        res.render("places/new", { message });
-      } else {
-        res.render("error404");
-      }
-    });
-});
+router.post('/:id/comment', (req, res) => {
+  console.log(req.body)
+  db.Place.findById(req.params.id)
+  .then(place => {
+      db.Comment.create(req.body)
+      .then(comment => {
+          place.comments.push(comment.id)
+          place.save()
+          .then(() => {
+              res.redirect(`/places/${req.params.id}`)
+          })
+      })
+      .catch(err => {
+          res.render('error404')
+      })
+  })
+  .catch(err => {
+      res.render('error404')
+  })
+})
 
 //NEW
 router.get("/new", (req, res) => {
@@ -45,16 +42,18 @@ router.get("/new", (req, res) => {
 });
 
 //GET BY ID
-router.get("/:id", (req, res) => {
-  db.Place.findById(req.params.id)
-    .then((place) => {
-      res.render("places/show", { place });
-    })
-    .catch((err) => {
-      console.log("err", err);
-      res.render("error404");
-    });
-});
+router.get('/:id', (req, res) => {
+  db.Place.findOne({ _id: req.params.id })
+      .populate('comments')
+      .then(place => {
+          console.log(place.comments)
+          res.render('places/show', { place })
+      })
+      .catch(err => {
+          console.log('err', err)
+          res.render('error404')
+      })
+})
 
 //UPDATE
 router.put('/:id', (req, res) => {
